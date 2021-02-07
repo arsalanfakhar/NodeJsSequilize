ae7195efbf6b47d1f0b2d511577cde1613571e42
Arsalan <arsalan.fakhar@yahoo.com>
2021-02-07 15:54:49 +0500

    add products table api

---
+++ app/controllers/tutorial_controller.js
@@ -0,0 +1,111 @@
+const db = require('../models/app');
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+        res.status(400).send({
+            message: "Title can not be empty!"
+        });
+        return;
+    }
+    // Create a Tutorial
+    const tutorial = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    };
+
+    Tutorial.create(tutorial)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Tutorial."
+            });
+        });
+};
+
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+
+    const title = req.query.title;
+    var condition = title ? {
+        title: {
+            [Op.like]: `%${title}%`
+        }
+    } : null;
+
+
+    Tutorial.findAll({
+            where: condition
+        })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500)
+                .send({
+                    message: err.message || "Some error occured while fetching"
+                });
+        });
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    Tutorial.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Error retrieving Tutorial with id=" + id
+            });
+        });
+};
+
+// Update a Tutorial by the id in the request
+exports.update = (req, res,next) => {
+    const id = req.params.id;
+
+    Tutorial.update(req.body, {
+            where: {
+                id: id
+            }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Tutorial was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Tutorial with id=" + id
+            });
+        });
+};
+
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+    
+};
+
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {
+
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app=express();
+
+// var corsOptions = {
+//     origin: "http://localhost:3306"
+//   };
+
+// app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+//parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const db=require('./app/models/app');
+db.sequelize.sync();
+
+// test route
+app.get('/test',(req,res,next)=>{
+    res.status(200).json('working');
+});
+
+
+require('./app/routes/turorial_routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+
+
